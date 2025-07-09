47b57554c23068e192adf37721c8adffccb4a82e
Erm2k8 <ermesson2k8and@gmail.com>
2025-07-09 15:17:42 -0300

    fix: error at create user

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
-from .forms import CustomUserCreationForm, CustomUserChangeForm 
+
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
-    
+class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'user_role', 'is_staff', 'is_active', 'date_joined')
     search_fields = ('email',)
     list_filter = ('is_staff', 'is_active', 'user_role', 'groups')
+    ordering = ('-date_joined',)
+    readonly_fields = ('last_login', 'date_joined')
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Informações Pessoais', {'fields': ('user_role', 'profile_picture')}), 
+        ('Informações Pessoais', {'fields': ('user_role', 'profile_picture')}),
         ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
     )
@@ -22,10 +20,11 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'user_role', 'password', 'password2'), 
+            'fields': ('email', 'user_role', 'password', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
     )
-    
-    readonly_fields = ('last_login', 'date_joined')
 
-    ordering = ('-date_joined',)
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.cleaned_data and form.cleaned_data['password']:
+            obj.set_password(form.cleaned_data['password'])
+        super().save_model(request, obj, form, change)
