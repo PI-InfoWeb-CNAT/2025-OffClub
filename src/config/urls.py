5720f235d39d2e9d86e6a5ad0ea89a3e66a04f71
Erm2k8 <ermesson2k8and@gmail.com>
2025-07-12 18:09:49 -0300

    feat: setup plans page

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.subscription.urls import urlpatterns as subscription_urls
+
 from django.shortcuts import render
 
 def index(request):
@@ -9,4 +11,5 @@ def index(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('subscription/', include(subscription_urls)),
 ]
