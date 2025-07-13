f9b68da760ebc34330813d0c654959e0b4b7794f
Erm2k8 <ermesson2k8and@gmail.com>
2025-07-13 20:03:43 -0300

    feat: implement reviews section

---
+++ src/apps/core/static/js/testimonials.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3.8,
+  spaceBetween: 10,
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
