0e96387628e7fe13d1d14bb94054d2ace50f94e6
ClaraTeoMorais <clara.teodosio@escolar.ifrn.edu.br>
2025-07-13 20:03:50 -0300

    Finalização da página de detalhar oferta  e resgatar cupom

--- src/static/js/offer_detail.js
+++ src/static/js/offer_detail.js
@@ -1,4 +1,36 @@
-document.getElementById('close-button-modal').addEventListener('click',janela);
-function janela(){
-    document.getElementById('offer-modal').style.display='none';
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const redeemButton = document.getElementById('redeem-button');
+    const accessCouponsButton = document.getElementById('ascess-coupons-button');
+    const successToast = document.getElementById('success-toast');
+    const closeToast = document.getElementById('close-toast');
+    const closeModalButton = document.getElementById('close-button-modal');
+    const transparentBg = document.getElementById('transparent-bg');
+    const offerModal = document.getElementById('offer-modal');
+
+    // Evento para esconder o modal
+    closeModalButton.addEventListener('click', function () {
+        offerModal.style.display = 'none';
+        transparentBg.style.display = 'none';
+    });
+
+    // Evento para fechar a notificação de sucesso
+    closeToast.addEventListener('click', function () {
+        successToast.style.display = 'none';
+    });
+
+    // Evento de clique no botão "RESGATAR"
+    redeemButton.addEventListener('click', function () {
+        // Mostra notificação de sucesso e botão de acessar cupons
+        accessCouponsButton.style.display = 'flex';
+        successToast.style.display = 'flex';
+
+        // Desativa botão visualmente e funcionalmente
+        redeemButton.classList.add('disabled');
+        redeemButton.disabled = true;
+
+        // Oculta notificação de sucesso após 5 segundos
+        setTimeout(() => {
+            successToast.style.display = 'none';
+        }, 5000);
+    });
+});
