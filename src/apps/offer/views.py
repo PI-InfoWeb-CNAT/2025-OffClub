6c75ea5c4900111fc74df78c4d8c9d8363815031
Heitor de França Barboza <20231011110036@ifrn.local>
2025-07-22 14:04:54 -0300

    starting offer views

--- src/apps/offer/views.py
+++ src/apps/offer/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views import View
 from ..offer.models import *
 
-def offer(request):
-    return render(request, 'offer_detail.html')
-
-class OfferViews:
+class OfferViews(View):
     @staticmethod
-    def list_filter_offer(request):
+    def list_filter_offer(self, request, *args, **kwargs):
         return render(request, template_name='offer.html', status=200)
+    @staticmethod
+    def offer(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            offers = Offer.objects.filter(end_date < timezone.now())
+            context = {'offers': offers}
+            return render(request, 'offer_detail.html', context)
 
