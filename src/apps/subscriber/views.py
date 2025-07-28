a17035257bcf1ab1180a22fa217ef728ac255649
Lucas <lucask4ssio@gmail.com>
2025-07-28 14:24:50 -0300

    Atualização na implementação

--- src/apps/subscriber/views.py
+++ src/apps/subscriber/views.py
@@ -15,17 +15,32 @@ class SubscriberViews(View):
             # user_id = logged_user.id
             # user_coupons = Coupon.objects.filter(subscriber=user_id)
             #context = {'coupons': user_coupons}
-            objects = Coupon.objects.all()
-            coupons = []
-            for coupon in objects:
+            used_filtered = Coupon.objects.filter(used_date__isnull=False).order_by('-used_date')
+            active_filtered = Coupon.objects.filter(used_date__isnull=True)
+            used_coupons = []
+            active_coupons = []
+            for coupon in used_filtered:
                 price = coupon.offer.price
                 discount = coupon.offer.discount
                 old_price, final_price = ServiceDiscount.final_price(price, discount)
                 coupon_data = {
                         'old_price':     old_price,
                         'final_price':   final_price,
+                        'used_month':    coupon.used_date.month,
                     }
                 dic = {'object': coupon, 'data': coupon_data}
-                coupons.append(dic)
-            context = {'coupons': coupons}
+                used_coupons.append(dic)
+
+            for coupon in active_filtered:
+                price = coupon.offer.price
+                discount = coupon.offer.discount
+                old_price, final_price = ServiceDiscount.final_price(price, discount)
+                coupon_data = {
+                        'old_price':     old_price,
+                        'final_price':   final_price,
+                        'used_month': None,
+                    }
+                dic = {'object': coupon, 'data': coupon_data}
+                active_coupons.append(dic)
+            context = {'used_coupons': used_coupons, 'active_coupons': active_coupons}
             return render(request, 'subscriber.html', context=context, status=200)
