483b31e51b6f4086e599f88c6b7852f896cb8855
Erm2k8 <ermesson2k8and@gmail.com>
2025-11-03 14:26:54 -0300

    feat: user profile

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +20,4 @@ urlpatterns = [
         name='offer_list'
     ),
     path('coupon/', include(('apps.coupon.urls'), namespace='coupon')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
